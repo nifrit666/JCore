02daeeb116a03ae9ebb71daf283b2e6e6dbceacd
«Andrey_Istomin» <an.istomin@gmail.com>
2024-07-11 00:26:00 +0500

    J Core init

---
+++ HW1/Task1/mydocumentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.gb.istomin","l":"Decorator"},{"p":"ru.gb.istomin","l":"Main"},{"p":"ru.gb.istomin","l":"OtherClass"}];updateSearchResults();
